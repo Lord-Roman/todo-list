f120320c661b8063ea76d79374d27029165e3985
Roman Korolev <rk89101459876@gmail.com>
2026-02-13 23:26:22 +0300

    add addTodo

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
 <template>
   <div class="container todo-app">
     <h1 class="title">Todo List</h1>
-    <div class="todo-app__main">
-      <ul class="todo-list" v-if="todos.length">
-        <li
-          class="todo-list__item"
-          :class="{ 'todo-list__item--completed': todo.completed }"
-          v-for="todo in todos"
-          :key="todo.id"
-        >
-          <span class="todo-list__item-text">{{ todo.text }}</span>
-          <button
-            class="btn btn--check"
-            :disabled="todo.completed"
-            @click="todo.completed = true"
-            aria-label="Завершить"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="15" height="22">
-              <path
-                d="M438.6 109.4c-12.5-12.5-32.8-12.5-45.3 0L160 320.7l-92.3-92.3c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0l288-288c12.6-12.5 12.6-32.8 .1-45.3z"
-              />
-            </svg>
-          </button>
-          <button class="btn btn--delete" @click="removeTodo(todo.id)" aria-label="Удалить">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="15" height="22">
-              <path
-                d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
-              />
-            </svg>
-          </button>
-        </li>
-      </ul>
-      <div class="todo-list__empty" v-else>
-        <p>Список задач пуст</p>
+    <form class="form">
+      <div class="form__group form__group--todo">
+        <input class="form-control" type="text" v-model="inputText" placeholder="Новая задача"/>
+        <button class="btn btn--add-todo" type="button" @click="addTodo">Добавить</button>
       </div>
-    </div>
-
+    </form>
+    <ToDoList
+      @remove-todo="removeTodo"
+      @complete="setCompleted" :todos="todos"
+    />
     <div class="todo-app__footer" v-if="todos.length">
       <p class="todo-app__footer-text">Осталось {{ todosNotCompletedCount }} задания(й)</p>
       <button class="btn btn--clear" @click="clearCompleted()">Удалить завершенные</button>
@@ -44,7 +19,10 @@
   </div>
 </template>
 <script setup>
-import { computed, reactive } from 'vue'
+import { computed, reactive, ref } from 'vue'
+import ToDoList from './components/ToDoList.vue'
+
+const inputText = ref('')
 
 const todos = reactive([
   {
@@ -64,9 +42,24 @@ const todos = reactive([
   },
 ])
 
+const addTodo = () => {
+  if(!inputText.value.length) {
+    return
+  }
+  const id = new Date();
+  const item = { id, text:inputText.value, completed: false }
+  todos.push(item)
+  inputText.value = ''
+}
+
 const removeTodo = (id) => {
   const index = todos.findIndex((item) => item.id === id)
   todos.splice(index, 1)
+}
+
+const setCompleted = (id) => {
+  const item = todos.find((item) => item.id === id)
+  item.completed = true
 }
 const todosNotCompletedCount = computed(() => todos.filter((item) => !item.completed).length)
 
